0e983b10e51be5d34620bdbaf22d04a2f91c7024
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-15 14:24:15 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+}]
+
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.status(200).send(`The ${article.name} article now has ${article.upvotes} upvotes`);
+    } else{
+        res.status(404).send('Article not found');
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000!');
+});
