f07c8927a8f1b9c89b1ff7bab35f5e31e875949f
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-15 15:34:32 +0530

    Connect mongo db

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -21,6 +24,19 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     if (article) {
         article.upvotes += 1;
         res.status(200).send(`The ${article.name} article now has ${article.upvotes} upvotes`);
+    } else{
+        res.status(404).send('Article not found');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { username, text } = req.body;
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.comments.push({ username, text });
+        res.status(200).send(article.comments);
     } else{
         res.status(404).send('Article not found');
     }
