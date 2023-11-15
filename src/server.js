ed4beb1dae705a242660db8dc0e00aa03815761f
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-16 00:49:42 +0530

    Add Firebase Admin and credentials.json, implement user authentication middleware, and update article upvoting and commenting logic.

--- src/server.js
+++ src/server.js
@@ -1,62 +1,101 @@
+import fs from 'fs';
+import admin from 'firebase-admin';
 import express from 'express';
-import {db, connectToDb} from './db.js';
+import { db, connectToDb } from './db.js';
+
+const credentials = JSON.parse(
+    fs.readFileSync('./credentials.json')
+);
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e) {
+            return res.sendStatus(400);
+        }
+    }
+
+    req.user = req.user || {};
+
+    next();
+});
+
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
-    if(article){
+    if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.includes(uid);
         res.json(article);
+    } else {
+        res.sendStatus(404);
     }
-    else{
-        res.status(404).json({ message: 'Article not found!' });
+});
+
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
     }
 });
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-
-    await db.collection('articles').updateOne({ name }, {
-        $inc: {
-            upvotes: 1,
-        },
-    });
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.status(200).json(article);
-    } else{
-        res.status(404).send('Article not found');
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.includes(uid);
+   
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },
+                $push: { upvoteIds: uid },
+            });
+        }
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle);
+    } else {
+        res.send('That article doesn\'t exist');
     }
 });
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { username, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: {
-            comments: { username, text },
-        },
+        $push: { comments: { username: email, text } },
     });
-
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.status(200).json(article);
-    } else{
-        res.status(404).send('Article not found');
+        res.json(article);
+    } else {
+        res.send('That article doesn\'t exist!');
     }
 });
 
 connectToDb(() => {
-    console.log('Successfully onnected to the database');
+    console.log('Successfully connected to database!');
     app.listen(8000, () => {
-        console.log('Server is listening on port 8000!');
+        console.log('Server is listening on port 8000');
     });
-});
+})
